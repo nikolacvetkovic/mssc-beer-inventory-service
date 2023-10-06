813abae976ac1fad4b255bb8b5eb04a63c54f6b1
Nikola <nikola.cvetkovic91@gmail.com>
2023-10-06 23:53:31 +0200

    change root package name, set port

--- src/main/java/xyz/riocode/beer/inventory/service/web/mappers/BeerInventoryMapper.java
+++ src/main/java/xyz/riocode/beer/inventory/service/web/mappers/BeerInventoryMapper.java
@@ -1,7 +1,7 @@
-package guru.sfg.beer.inventory.service.web.mappers;
+package xyz.riocode.beer.inventory.service.web.mappers;
 
-import guru.sfg.beer.inventory.service.domain.BeerInventory;
-import guru.sfg.beer.inventory.service.web.model.BeerInventoryDto;
+import xyz.riocode.beer.inventory.service.domain.BeerInventory;
+import xyz.riocode.beer.inventory.service.web.model.BeerInventoryDto;
 import org.mapstruct.Mapper;
 
 /**
