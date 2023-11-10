93ca18f396526c9e4ef1372333ab8fcb74d81a2c
Nikola <nikola.cvetkovic91@gmail.com>
2023-11-10 15:55:13 +0100

    fix packages

--- src/test/java/xyz/riocode/brewery/beer/inventory/service/BeerInventoryApplicationTests.java
+++ src/test/java/xyz/riocode/brewery/beer/inventory/service/BeerInventoryApplicationTests.java
@@ -1,4 +1,4 @@
-package xyz.riocode.beer.inventory.service;
+package xyz.riocode.brewery.beer.inventory.service;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
